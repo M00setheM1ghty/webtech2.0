249d1460427589d9e92392ab5972ad415e532983
M00setheM1ghty <129610686+M00setheM1ghty@users.noreply.github.com>
2023-12-29 10:42:50 +0100

    moved db connection to config file

--- config/dbaccess.php
+++ config/dbaccess.php
@@ -2,5 +2,13 @@
 $servername = "localhost";
 $username = "thomas";
 $password = "Tommy97xampp#";
-$database = "webtechproject";
+$database = "db_hotel";
+
+$db_obj = new mysqli($servername, $username, $password, $database);
+
+        if ($db_obj->connect_error) {
+            echo "Connection Error: " . $db_obj->connect_error;
+            exit();
+        }
 ?>
+

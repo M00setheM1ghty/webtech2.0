d59909b49aeeb465579b860cebc228e2e50b46f8
M00setheM1ghty <129610686+M00setheM1ghty@users.noreply.github.com>
2024-01-03 12:46:14 +0100

    added display of all users in admin page

---
+++ assets/components/display_userlist.php
@@ -0,0 +1,43 @@
+<!-- table with users -->
+<table class="table">
+    <thead>
+        <tr>
+            <th>Vorname</th>
+            <th>Nachname</th>
+            <th>Email</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        // Fetch reservations for the current user based on email
+        require_once('../../config/dbaccess.php');
+        $get_users_query = "
+                            SELECT `nachname`, `vorname`, `email`
+                            FROM `user_profil`";
+        $fetch_users = $db_obj->prepare($get_users_query);
+        $fetch_users->execute();
+        // put results in an assoc array
+        $fetched_users_array = $fetch_users->get_result();
+
+        if ($debug) {
+            if ($fetch_data->error) {
+                echo "Query error: " . $fetch_data->error;
+            }
+
+        }
+        // Loop through reservations and display in the table
+        while ($row = $fetched_users_array->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td>{$row['vorname']}</td>";
+            echo "<td>{$row['nachname']}</td>";
+            echo "<td>{$row['email']}</td>";
+            echo "</tr>";
+        }
+        if ($debug) {
+            echo "Session variables:";
+            var_dump($_SESSION);
+        }
+        ?>
+    </tbody>
+</table>
+</div>
